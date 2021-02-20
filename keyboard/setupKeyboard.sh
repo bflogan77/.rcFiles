b3ec96020d6dcf7a212774b53f3c291cd213631f
bflogan77 <brock.f.logan@gmail.com>
2021-02-20 13:58:05 -0600

    works i think

--- keyboard/setupKeyboard.sh
+++ keyboard/setupKeyboard.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-cp ~/.rcFiles/keyboard/usv /usr/share/X11/xkb/symbols/usv
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cp "${DIR}/usv" /usr/share/X11/xkb/symbols/usv
 cd /usr/share/X11/xkb/symbols/
 setxkbmap usv
 localectl set-x11-keymap usv
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-ln -sf ~/.rcFiles/.*rc ~/
-ln -sf ~/.rcFiles/*.conf ~/
-bash ./keyboard/setupKeyboard.sh
+dir="$(cd "$(dirname "$0")" && pwd)"
+dirDotDot="$(dirname dir)"
+ln -sf ${dir}/.rcFiles/.*rc ${dirDotDot}/
+ln -sf ${dir}/.rcFiles/.*.conf ${dirDotDot}/
+bash ${dir}/keyboard/setupKeyboard.sh
