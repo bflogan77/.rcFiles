677154090906b225df68b28198a37a38b96256c2
bflogan77 <brock.f.logan@gmail.com>
2021-03-02 09:47:02 -0600

    starting cheat sheets and other changes

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 dir="$(cd "$(dirname "$0")" && pwd)"
 dirDotDot="$(dirname dir)"
+chsh -s /bin/bash
 ln -sf ${dir}/.rcFiles/.*rc ${dirDotDot}/
 ln -sf ${dir}/.rcFiles/.*.conf ${dirDotDot}/
 bash ${dir}/keyboard/setupKeyboard.sh
