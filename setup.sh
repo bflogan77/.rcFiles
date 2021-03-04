2ad87bfb5123f96fbe8d6c316c5a2c98a59eb93f
bflogan77 <brock.f.logan@gmail.com>
2021-03-03 22:12:03 -0600

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
 dir="$(cd "$(dirname "$0")" && pwd)"
-dirDotDot="$(dirname dir)"
+dirDotDot="$(dirname $dir)"
+echo $dir
+echo $dirDotDot
 chsh -s /bin/bash
 ln -sf ${dir}/.rcFiles/.*rc ${dirDotDot}/
 ln -sf ${dir}/.rcFiles/.*.conf ${dirDotDot}/
+ln -sf ${dir}/.rcFiles/.profile ${dirDotDot}/
+
 bash ${dir}/keyboard/setupKeyboard.sh
