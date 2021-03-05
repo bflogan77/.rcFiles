c44b5d5e35b1ea558c6bda2e42c89d877ce43702
bflogan77 <brock.f.logan@gmail.com>
2021-03-04 22:01:13 -0600

    setup.sh correct

--- setup.sh
+++ setup.sh
@@ -2,11 +2,9 @@
 
 dir="$(cd "$(dirname "$0")" && pwd)"
 dirDotDot="$(dirname $dir)"
-echo $dir
-echo $dirDotDot
 chsh -s /bin/bash
-ln -sf ${dir}/.rcFiles/.*rc ${dirDotDot}/
-ln -sf ${dir}/.rcFiles/.*.conf ${dirDotDot}/
-ln -sf ${dir}/.rcFiles/.profile ${dirDotDot}/
+ln -sf ${dir}/.*rc ${dirDotDot}/
+ln -sf ${dir}/.*.conf ${dirDotDot}/
+ln -sf ${dir}/.profile ${dirDotDot}/
 
 bash ${dir}/keyboard/setupKeyboard.sh
